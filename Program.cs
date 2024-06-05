0f74c9cedf30afdc03d45929476eb79924af0c60
Oliver Heil <git@heilbit.de>
2024-06-05 10:54:44 +0200

    - build target now .net8.0 - Changes for current pwnedpasswords.txt format where lines have variable length because after each hash the count of occurences has been added, delimiter is ":". - Password input does not echo  the password anymore => passwords are never visible, length of password still visible - If password itself is a SHA1 hash (check if it is 40 bytes long), additional search for password - Some minor changes in text output

--- Program.cs
+++ Program.cs
@@ -8,43 +8,103 @@ namespace CheckPwnedPasswords
 {
     class Program
     {
+        //Length of the SHA1 hash
+        const int SHA1LENGTH = 40;
+
         static void Main(string[] args)
 		{
 			List<string> filesToSearch = ProcessArguments(args);
 
-			while (true)
+            static string ByteArrayToString(byte[] ba)
+            {
+                string hex = BitConverter.ToString(ba);
+                return hex.Replace("-", "");
+            }
+
+            byte[] bytes;
+            System.Security.Cryptography.SHA1 sha;
+            byte[] passwordBytes;
+            string asHex;
+
+            while (true)
 			{
-				Console.WriteLine("");
+				//Console.WriteLine("");
 				Console.WriteLine("");
 				Console.WriteLine("Enter password (leave empty to quit):");
-				var pwd = Console.ReadLine();
-				if (string.IsNullOrWhiteSpace(pwd)) return;
 
-				foreach (var file in filesToSearch)
+                //var pwd = Console.ReadLine();
+                var pwd = string.Empty;
+                ConsoleKey key;
+                do
+                {
+                    var keyInfo = Console.ReadKey(intercept: true);
+                    key = keyInfo.Key;
+
+                    if (key == ConsoleKey.Backspace && pwd.Length > 0)
+                    {
+                        Console.Write("\b \b");
+                        pwd = pwd[0..^1];
+                    }
+                    else if (!char.IsControl(keyInfo.KeyChar))
+                    {
+                        Console.Write("*");
+                        pwd += keyInfo.KeyChar;
+                    }
+                } while (key != ConsoleKey.Enter);
+                Console.Write("\n");
+
+                if (string.IsNullOrWhiteSpace(pwd)) return;
+
+                bytes = System.Text.Encoding.UTF8.GetBytes(pwd);
+                sha = System.Security.Cryptography.SHA1.Create();
+                passwordBytes = sha.ComputeHash(bytes);
+                asHex = ByteArrayToString(passwordBytes);
+                Console.WriteLine($"Password SHA1 hash is: {asHex}");
+
+                foreach (var file in filesToSearch)
 				{
 					var sw = new Stopwatch();
 					sw.Start();
-					var result = Check(pwd, file);
+					var result = Check(asHex, file);
 					sw.Stop();
-
 
 					if (result)
 					{
-						Console.WriteLine($"{pwd} is in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
+                        Console.WriteLine($"Found in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
 						break;
 					}
 					else
 					{
-						Console.WriteLine($"{pwd} is NOT in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
+						Console.WriteLine($"NOT found in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
 					}
-				}
+
+                    if(pwd.Length == SHA1LENGTH)
+                    {
+                        //pwd is perhaps already a SHA1 hash so check it too
+                        Console.WriteLine($"Looking for password as it looks like a SHA hash itself ({SHA1LENGTH} bytes long)");
+
+                        sw.Reset();
+                        sw.Start();
+                        result = Check(pwd, file);
+                        sw.Stop();
+                        if (result)
+                        {
+                            Console.WriteLine($"Found hash in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Hash NOT found in {file} - time taken was {sw.Elapsed.Milliseconds}ms");
+                        }
+                    }
+                }
 			}
 		}
 
 		private static List<string> ProcessArguments(string[] args)
 		{
-			List<string> filesToSearch = new List<string>();
-			List<string> directoriesToSearch = new List<string>();
+			List<string> filesToSearch = [];
+			List<string> directoriesToSearch = [];
 
 			string baseDataDir = Path.Combine(AppContext.BaseDirectory, "data");
 			if (Directory.Exists(baseDataDir))
@@ -76,58 +136,66 @@ namespace CheckPwnedPasswords
 			return filesToSearch;
 		}
 
-		static bool Check(string password, string filename)
+		static bool Check(string asHex, string filename)
         {
-            string ByteArrayToString(byte[] ba)
+            var buffer = new byte[SHA1LENGTH];
+            using var sr = File.OpenRead(filename);
+            
+            //Number of lines
+            //var high = (sr.Length / (LINELENGTH + 2)) - 1;
+            //As line length is now variable just use the bytes in the file:
+            long high = sr.Length;
+            long low = 0L;
+
+            int ch;
+
+            while (low <= high)
             {
-                string hex = BitConverter.ToString(ba);
-                return hex.Replace("-", "");
-            }
+                long middle = (low + high + 1) / 2;
+                //Console.WriteLine($"{low}:{middle}:{high}");
+                
+                //sr.Seek((LINELENGTH + 2) * ((long)middle), SeekOrigin.Begin);
 
-            var bytes = System.Text.Encoding.UTF8.GetBytes(password);
-            var sha = System.Security.Cryptography.SHA1.Create();
-            var passwordBytes = sha.ComputeHash(bytes);
-            var asHex = ByteArrayToString(passwordBytes);
+				//seek to next cr/newline (0d0a) ahead of current position
+                bool do_seek = true;
+				while (middle >= low+1 && do_seek)
+				{
+					middle -= 1;
+					sr.Seek(middle, SeekOrigin.Begin);
+					ch = sr.ReadByte();
+					if (ch == 0x0d)
+					{
+						ch = sr.ReadByte();
+						if (ch == 0x0a && middle < sr.Length - 1)
+						{
+							middle += 2;
+							do_seek = false;
+						}
+					}
+				}
+				sr.Seek(middle, SeekOrigin.Begin);
 
-            //Length of the SHA1 hash  (we use LINELENGTH+2 is take the newline characters into account)
-            const int LINELENGTH = 40;
+                sr.Read(buffer, 0, SHA1LENGTH);
+                var readLine = Encoding.ASCII.GetString(buffer);
 
-            var buffer = new byte[LINELENGTH];
-            using (var sr = File.OpenRead(filename))
-            {
-                //Number of lines
-                var high = (sr.Length / (LINELENGTH + 2)) - 1;
-                var low = 0L;
-
-                while (low <= high)
+                switch (readLine.CompareTo(asHex))
                 {
-                    var middle = (low + high + 1) / 2;
-                    sr.Seek((LINELENGTH + 2) * ((long)middle), SeekOrigin.Begin);
-                    sr.Read(buffer, 0, LINELENGTH);
-                    var readLine = Encoding.ASCII.GetString(buffer);
+                    case 0:
+                        return true;
 
-                    switch (readLine.CompareTo(asHex))
-                    {
-                        case 0:
-                            return true;
+                    case 1:
+                        high = middle - 1;
+                        break;
 
-                        case 1:
-                            high = middle - 1;
-                            break;
+                    case -1:
+                        low = middle + 1;
+                        break;
 
-                        case -1:
-                            low = middle + 1;
-                            break;
-
-                        default:
-                            break;
-                    }
+                    default:
+                        break;
                 }
-
             }
-
             return false;
-
         }
     }
 }
